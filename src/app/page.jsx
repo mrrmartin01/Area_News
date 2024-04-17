a0d86704a1b4459e8f550274c57fb282de7bb415
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-17 02:51:26 +0100

    styling the homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,36 @@
+import Image from "next/image";
 import styles from "./home.module.css";
 function page() {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1>
-          Welcome to our corner of the internet! Get cozy and explore our latest
-          articles.
-        </h1>
+        <h1>Welcome to Area News! Your hub for all things local</h1>
+        <p>
+          Dive in and discover what&apos;s happening in your area! Stay
+          informed, stay connected.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn more</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={StyleSheetList.brands}>
+          <Image
+            src={"/brands.png"}
+            alt="brands"
+            className={styles.branding}
+            fill
+          />
+        </div>
       </div>
-      <div className={styles.imageContainer}></div>
+      <div className={styles.imageContainer}>
+        <Image
+          src={"/hero.gif"}
+          alt="hero"
+          className={styles.heroImg}
+          fill
+          // sizes="(max-width: 480px) 120px, (min-width: 481px) and (max-width: 1024px) 120px, 150px"
+        />
+      </div>
     </div>
   );
 }
