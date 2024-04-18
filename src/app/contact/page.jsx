df439fbdde65f66978ee728de20e763277c0868c
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-18 20:57:30 +0100

    contact page done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const ContactPage = () => {
   return (
-    <div>
-      Contact
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={"/contact.png"} alt="contact" fill className={styles.img}/>
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Please enter your name" />
+          <input type="text" placeholder="example@email.com" />
+          <input type="text" placeholder="+123 24 678 9000" />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Type message here"></textarea>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
