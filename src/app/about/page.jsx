dfcb6869dff2814a3171b0c7118708bf3ba8086a
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-18 20:14:00 +0100

    home and about page done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,9 +4,37 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Area News</h2>
+        <h1 className={styles.title}>
+          We serve you with the hottest news and gist within the
+          &ldquo;hood&ldquo;
+        </h1>
+        <p className={styles.desc}>
+          We provide digital news that are bigger, bolder adn the best. We
+          believe in news that accuracy, flexibility and precision. We&apos;re
+          on a journey to become the wolrd&apos;s best counsulting & finance
+          solution provider. A wide range of software and web solutions to our
+          suites of services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10K +</h1>
+            <p>Likes</p>
+          </div>
+          <div className={styles.box}>
+            <h1>150K +</h1>
+            <p>Shares</p>
+          </div>
+          <div className={styles.box}>
+            <h1>500K +</h1>
+            <p>Subscription</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src={"/about.png"} alt="about" fill />
+        <Image src={"/about.png"} alt="about" fill className={styles.img} />
       </div>
     </div>
   );
