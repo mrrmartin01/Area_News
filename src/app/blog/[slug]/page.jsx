2b9aba26bda1c4582d16ad48dd04cac8413a4ad4
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-19 08:09:12 +0100

    linking mongosh

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
+import PostUser from "../../../components/postUser/PostUser";
+import { Suspense } from "react";
 
 const getData = async (slug) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`); 
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
   if (!res.ok) {
     throw new Error("No post found");
   }
   return res.json();
 };
 
-
-const SingleBlog = async({params}) => {
-  const {slug} = params;
+const SingleBlog = async ({ params }) => {
+  const { slug } = params;
 
   const post = await getData(slug);
-
 
   return (
     <div className={styles.container}>
@@ -24,19 +24,22 @@ const SingleBlog = async({params}) => {
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image className={styles.avatar} src={"/blog.png"} alt="" width={50} height={50} />
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>John Doe</span>
-          </div>
+          <Image
+            className={styles.avatar}
+            src={"/blog.png"}
+            alt=""
+            width={50}
+            height={50}
+          />
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostUser userId={post.userId} />
+          </Suspense>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>4.23.2022</span>
           </div>
         </div>
-        <div className={styles.content}>
-          {post.body}
-        </div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
