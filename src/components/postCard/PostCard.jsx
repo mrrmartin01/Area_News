92358b86e9badb69f338934c97308fda19cfa109
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-19 06:42:26 +0100

    dynamic fetching of post

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./PostCard.module.css";
 import Link from "next/link";
 
-const PostCard = () => {
+const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -12,12 +12,9 @@ const PostCard = () => {
         <span className={styles.date}>12.03.2002</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <p className={styles.desc}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-          laboriosam distinctio harum nisi vitae eveniet ad aliquid illo
-          praesentium. Beatae facere illo modi delectus error provident, numquam
-          deleniti porro animi.
+          {post.body}
         </p>
         <Link className={styles.link} href={"blog/post"}>
           READ MORE
