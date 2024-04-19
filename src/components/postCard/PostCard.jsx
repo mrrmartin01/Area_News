8e5bb57386ac0f579f52a66d75df2363d38f9a8e
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-19 03:36:44 +0100

    postCard complete

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -1,23 +1,30 @@
-import Image from 'next/image'
-import styles from './PostCard.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "./PostCard.module.css";
+import Link from "next/link";
 
 const PostCard = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-            <Image src={"/favicon2.ico"} alt='' fill className={styles.img}/>
+          <Image src={"/blog.png"} alt="" fill className={styles.img} />
         </div>
         <span className={styles.date}>12.03.2002</span>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>Description</p>
-        <Link className={styles.link} href={'blog/post'}>READ MORE</Link>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+          laboriosam distinctio harum nisi vitae eveniet ad aliquid illo
+          praesentium. Beatae facere illo modi delectus error provident, numquam
+          deleniti porro animi.
+        </p>
+        <Link className={styles.link} href={"blog/post"}>
+          READ MORE
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
