88c818db9e74d82faa84bc5360d27ab2590c8977
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-22 02:28:25 +0100

    user ui edit

--- src/components/postUser/PostUser.jsx
+++ src/components/postUser/PostUser.jsx
@@ -1,26 +1,23 @@
-import { getUser } from "../../lib/data";
-import styles from "./postUser.module.css";
-import Image from "next/image";
+import { getUsers } from "../../lib/data";
+import styles from "./PostUser.module.css";
 
 
+// const getData = async ({userId}) => {
+//     const res = await getUsers( {cache: "no-store"}); 
+//     if (!res.ok) {
+//       throw new Error("No post found");
+//     }
+//     return res.json();
+//   };
 
-const PostUser = async ({ userId }) => {
 
-  const user = await getUser(userId);
+const PostUser = async ({userId}) => {
+    const user = await getUsers(userId) 
 
   return (
     <div className={styles.container}>
-      <Image
-        className={styles.avatar}
-        src={user.img ? user.img : "/noavatar.png"}
-        alt=""
-        width={50}
-        height={50}
-      />
-      <div className={styles.texts}>
-        <span className={styles.title}>Author</span>
-        <span className={styles.username}>{user.username}</span>
-      </div>
+      <span className={styles.title}>Author</span>
+      <span className={styles.username }>{user.name}</span>
     </div>
   );
 };
