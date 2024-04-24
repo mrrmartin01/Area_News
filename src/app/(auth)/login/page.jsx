742a6c12ae9285bec6ebe57a7e8c8ba7eb591766
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-04-24 14:43:13 +0100

    githun auth added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { signIn } from '../../../lib/auth'
 
 const LoginPage = () => {
+  const handleGithubLogin= async () => {
+    "use server"
+
+    await signIn("github");
+  }
+
   return (
     <div>
-      Login
+      <form action={handleGithubLogin}>
+        <button>Login with GitHub</button>
+      </form>
     </div>
   )
 }
